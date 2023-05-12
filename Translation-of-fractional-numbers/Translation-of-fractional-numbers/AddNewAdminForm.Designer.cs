974850034668b16b63fd052e3b8f62615f4ac52d
Riccids <marcericcid@gmail.com>
2023-05-12 16:51:46 +0300

    Добавлен прототип интерфейса программы v0.1

---
+++ Translation-of-fractional-numbers/Translation-of-fractional-numbers/AddNewAdminForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Translation_numbers
+{
+    partial class AddNewAdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.adminNameBox = new System.Windows.Forms.TextBox();
+            this.adminSurnameBox = new System.Windows.Forms.TextBox();
+            this.adminLoginBox = new System.Windows.Forms.TextBox();
+            this.adminPasswordBox = new System.Windows.Forms.TextBox();
+            this.acceptButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добавление нового администратора";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Фамилия: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Логин: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Пароль: ";
+            // 
+            // adminNameBox
+            // 
+            this.adminNameBox.Location = new System.Drawing.Point(157, 70);
+            this.adminNameBox.Name = "adminNameBox";
+            this.adminNameBox.Size = new System.Drawing.Size(213, 22);
+            this.adminNameBox.TabIndex = 5;
+            // 
+            // adminSurnameBox
+            // 
+            this.adminSurnameBox.Location = new System.Drawing.Point(157, 115);
+            this.adminSurnameBox.Name = "adminSurnameBox";
+            this.adminSurnameBox.Size = new System.Drawing.Size(213, 22);
+            this.adminSurnameBox.TabIndex = 6;
+            // 
+            // adminLoginBox
+            // 
+            this.adminLoginBox.Location = new System.Drawing.Point(157, 159);
+            this.adminLoginBox.Name = "adminLoginBox";
+            this.adminLoginBox.Size = new System.Drawing.Size(213, 22);
+            this.adminLoginBox.TabIndex = 7;
+            // 
+            // adminPasswordBox
+            // 
+            this.adminPasswordBox.Location = new System.Drawing.Point(157, 204);
+            this.adminPasswordBox.Name = "adminPasswordBox";
+            this.adminPasswordBox.Size = new System.Drawing.Size(213, 22);
+            this.adminPasswordBox.TabIndex = 8;
+            // 
+            // acceptButton
+            // 
+            this.acceptButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.acceptButton.Location = new System.Drawing.Point(466, 312);
+            this.acceptButton.Name = "acceptButton";
+            this.acceptButton.Size = new System.Drawing.Size(154, 33);
+            this.acceptButton.TabIndex = 9;
+            this.acceptButton.Text = "Подвердить";
+            this.acceptButton.UseVisualStyleBackColor = true;
+            // 
+            // AddNewAdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(632, 357);
+            this.Controls.Add(this.acceptButton);
+            this.Controls.Add(this.adminPasswordBox);
+            this.Controls.Add(this.adminLoginBox);
+            this.Controls.Add(this.adminSurnameBox);
+            this.Controls.Add(this.adminNameBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AddNewAdminForm";
+            this.Text = "AddNewAdminForm";
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox adminNameBox;
+        private System.Windows.Forms.TextBox adminSurnameBox;
+        private System.Windows.Forms.TextBox adminLoginBox;
+        private System.Windows.Forms.TextBox adminPasswordBox;
+        private System.Windows.Forms.Button acceptButton;
+    }
+}
---
+++ Translation-of-fractional-numbers/Translation-of-fractional-numbers/ProfileEditForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Translation_of_fractional_numbers
+{
+    partial class ProfileEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.editNameBox = new System.Windows.Forms.TextBox();
+            this.EditSurnameBox = new System.Windows.Forms.TextBox();
+            this.editLoginBox = new System.Windows.Forms.TextBox();
+            this.userPasswordBox = new System.Windows.Forms.TextBox();
+            this.editPasswordBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(73, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Редактирования профиля";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(73, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(73, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Фамилия:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(73, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Новый логин:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(73, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Текущий пароль:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(73, 295);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Новый пароль:";
+            // 
+            // editBtn
+            // 
+            this.editBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.editBtn.Location = new System.Drawing.Point(540, 365);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(135, 37);
+            this.editBtn.TabIndex = 6;
+            this.editBtn.Text = "Подтвердить";
+            this.editBtn.UseVisualStyleBackColor = true;
+            // 
+            // editNameBox
+            // 
+            this.editNameBox.Location = new System.Drawing.Point(258, 70);
+            this.editNameBox.Name = "editNameBox";
+            this.editNameBox.Size = new System.Drawing.Size(136, 22);
+            this.editNameBox.TabIndex = 7;
+            // 
+            // EditSurnameBox
+            // 
+            this.EditSurnameBox.Location = new System.Drawing.Point(258, 122);
+            this.EditSurnameBox.Name = "EditSurnameBox";
+            this.EditSurnameBox.Size = new System.Drawing.Size(136, 22);
+            this.EditSurnameBox.TabIndex = 8;
+            // 
+            // editLoginBox
+            // 
+            this.editLoginBox.Location = new System.Drawing.Point(258, 173);
+            this.editLoginBox.Name = "editLoginBox";
+            this.editLoginBox.Size = new System.Drawing.Size(136, 22);
+            this.editLoginBox.TabIndex = 9;
+            // 
+            // userPasswordBox
+            // 
+            this.userPasswordBox.Location = new System.Drawing.Point(258, 235);
+            this.userPasswordBox.Name = "userPasswordBox";
+            this.userPasswordBox.Size = new System.Drawing.Size(136, 22);
+            this.userPasswordBox.TabIndex = 10;
+            // 
+            // editPasswordBox
+            // 
+            this.editPasswordBox.Location = new System.Drawing.Point(258, 299);
+            this.editPasswordBox.Name = "editPasswordBox";
+            this.editPasswordBox.Size = new System.Drawing.Size(136, 22);
+            this.editPasswordBox.TabIndex = 11;
+            // 
+            // ProfileEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(698, 421);
+            this.Controls.Add(this.editPasswordBox);
+            this.Controls.Add(this.userPasswordBox);
+            this.Controls.Add(this.editLoginBox);
+            this.Controls.Add(this.EditSurnameBox);
+            this.Controls.Add(this.editNameBox);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ProfileEditForm";
+            this.Text = "ProfileEditForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.TextBox editNameBox;
+        private System.Windows.Forms.TextBox EditSurnameBox;
+        private System.Windows.Forms.TextBox editLoginBox;
+        private System.Windows.Forms.TextBox userPasswordBox;
+        private System.Windows.Forms.TextBox editPasswordBox;
+    }
+}
